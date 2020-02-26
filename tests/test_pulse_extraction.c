cf3cad205234a685a80339b2141ba61c784b4dc9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-02-26 18:13:24 +0100

    test arrival time spread

--- tests/test_pulse_extraction.c
+++ tests/test_pulse_extraction.c
@@ -1,97 +1,111 @@
 
 CASE("truncate_invalid_arrival_times") {
-    struct mliMT19937 prng = mliMT19937_init(0);
-    const double time_slice_duration = 500e-12;
-    const double arrival_time_std = 0.0;
-    uint64_t num_invalid_photons = 0u;
-    uint64_t num_passing_photons = 0u;
-    int64_t i, p, ph;
+        struct mliMT19937 prng = mliMT19937_init(0);
+        const double time_slice_duration = 500e-12;
+        const double arrival_time_std = 0.0;
+        uint64_t num_invalid_photons = 0u;
+        uint64_t num_passing_photons = 0u;
+        int64_t i, p, ph;
 
-    struct mlispPulseVector pulse_pipeline = mlispPulseVector_init();
-    struct mlispExtractedPulseVector extraction_pipeline;
+        struct mlispPulseVector pulse_pipeline = mlispPulseVector_init();
+        struct mlispExtractedPulseVector extraction_pipeline;
 
-    CHECK(mlispPulseVector_malloc(&pulse_pipeline, 0u));
-    for (i = -1000; i < 1000; i++) {
-        struct mlispPulse pulse;
-        pulse.arrival_time = time_slice_duration*(double)i;
-        pulse.simulation_truth_id = 0;
-        CHECK(mlispPulseVector_push_back(&pulse_pipeline, pulse));
-    }
-
-    num_invalid_photons = 0u;
-    for (p = 0; p < pulse_pipeline.vector.size; p++) {
-        struct mlispPulse pulse = mlispPulseVector_at(&pulse_pipeline, p);
-        int32_t slice = mli_near_int(pulse.arrival_time/time_slice_duration);
-
-        if (slice < 0 || slice >= MLISP_NUM_TIME_SLICES) {
-            num_invalid_photons++;
+        CHECK(mlispPulseVector_malloc(&pulse_pipeline, 0u));
+        for (i = -1000; i < 1000; i++) {
+                struct mlispPulse pulse;
+                pulse.arrival_time = time_slice_duration*(double)i;
+                pulse.simulation_truth_id = 0;
+                CHECK(mlispPulseVector_push_back(&pulse_pipeline, pulse));
         }
-    }
 
-    CHECK(2000 - MLISP_NUM_TIME_SLICES == num_invalid_photons);
+        num_invalid_photons = 0u;
+        for (p = 0; p < pulse_pipeline.vector.size; p++) {
+                struct mlispPulse pulse = mlispPulseVector_at(&pulse_pipeline, p);
+                int32_t slice = mli_near_int(pulse.arrival_time/time_slice_duration);
 
-    extraction_pipeline = mlispExtractedPulseVector_init();
-    CHECK(mlispExtractedPulseVector_malloc(&extraction_pipeline, 0u));
-    CHECK(mlisp_extract_pulses(
-        &pulse_pipeline,
-        &extraction_pipeline,
-        time_slice_duration,
-        MLISP_NUM_TIME_SLICES,
-        arrival_time_std,
-        &prng));
+                if (slice < 0 || slice >= MLISP_NUM_TIME_SLICES) {
+                        num_invalid_photons++;
+                }
+        }
 
-    CHECK(pulse_pipeline.vector.size != extraction_pipeline.vector.size);
-    num_passing_photons = 0;
-    for (ph = 0; ph < extraction_pipeline.vector.size; ph++) {
-            struct mlispExtractedPulse expulse = mlispExtractedPulseVector_at(
-            &extraction_pipeline,
-            ph);
-        CHECK(255 > expulse.arrival_time_slice);
-        num_passing_photons++;
-    }
+        CHECK(2000 - MLISP_NUM_TIME_SLICES == num_invalid_photons);
 
-    CHECK(MLISP_NUM_TIME_SLICES == num_passing_photons);
-    mlispExtractedPulseVector_free(&extraction_pipeline);
-    mlispPulseVector_free(&pulse_pipeline);
+        extraction_pipeline = mlispExtractedPulseVector_init();
+        CHECK(mlispExtractedPulseVector_malloc(&extraction_pipeline, 0u));
+        CHECK(mlisp_extract_pulses(
+                &pulse_pipeline,
+                &extraction_pipeline,
+                time_slice_duration,
+                MLISP_NUM_TIME_SLICES,
+                arrival_time_std,
+                &prng));
+
+        CHECK(pulse_pipeline.vector.size != extraction_pipeline.vector.size);
+        num_passing_photons = 0;
+        for (ph = 0; ph < extraction_pipeline.vector.size; ph++) {
+                struct mlispExtractedPulse expulse = mlispExtractedPulseVector_at(
+                    &extraction_pipeline,
+                    ph);
+                CHECK(255 > expulse.arrival_time_slice);
+                num_passing_photons++;
+        }
+
+        CHECK(MLISP_NUM_TIME_SLICES == num_passing_photons);
+        mlispExtractedPulseVector_free(&extraction_pipeline);
+        mlispPulseVector_free(&pulse_pipeline);
 }
 
-/*
 CASE("arrival_time_std") {
-    struct mliMT19937 prng = mliMT19937_init(0);
-    const double time_slice_duration = 500e-12;
-    const double arrival_time_std = 5e-9;
-    const double true_arrival_time = 25e-9;
+        struct mliMT19937 prng = mliMT19937_init(0);
+        const double time_slice_duration = 500e-12;
+        const double arrival_time_std = 5e-9;
+        const double true_arrival_time = 25e-9;
+        uint64_t i;
+        uint64_t max_num_time_slices = 100;
+        struct mlispPulseVector pulse_pipeline = mlispPulseVector_init();
+        struct mlispExtractedPulseVector extraction_pipeline =
+                mlispExtractedPulseVector_init();
+        double *reconstructed_arrival_times = NULL;
+        double reco_mean;
+        double reco_std;
+        int64_t num_reco;
+        CHECK(mlispPulseVector_malloc(&pulse_pipeline, 0u));
 
-    std::vector<std::vector<signal_processing::ElectricPulse>> response;
-    std::vector<signal_processing::ElectricPulse> pulse_pipeline;
-    for (int i = 0; i < 10*1000; i++) {
-        signal_processing::ElectricPulse pulse;
-        pulse.arrival_time = true_arrival_time;
-        pulse.simulation_truth_id = i;
-        pulse_pipeline.push_back(pulse);
-    }
-    response.push_back(pulse_pipeline);
+        for (i = 0; i < 10*1000; i++) {
+            struct mlispPulse pulse;
+            pulse.arrival_time = true_arrival_time;
+            pulse.simulation_truth_id = i;
+            CHECK(mlispPulseVector_push_back(&pulse_pipeline, pulse));
+        }
 
-    std::vector<double> true_arrival_times;
-    for (signal_processing::ElectricPulse &pulse : response.at(0))
-        true_arrival_times.push_back(pulse.arrival_time);
+        CHECK(mlispExtractedPulseVector_malloc(&extraction_pipeline, 0u));
 
-    CHECK(0.0 == Approx(ml::numeric::stddev(true_arrival_times)).margin(1e-1));
-    CHECK(true_arrival_time == Approx(ml::numeric::mean(true_arrival_times)).margin(1e-1));
+        CHECK(mlisp_extract_pulses(
+                &pulse_pipeline,
+                &extraction_pipeline,
+                time_slice_duration,
+                max_num_time_slices,
+                arrival_time_std,
+                &prng));
 
-    std::vector<std::vector<signal_processing::ExtractedPulse>> raw =
-        signal_processing::extract_pulses(
-            response,
-            time_slice_duration,
-            arrival_time_std,
-            &prng);
+        num_reco = extraction_pipeline.vector.size;
+        reconstructed_arrival_times = (double *)malloc(sizeof(double)*num_reco);
+        CHECK(reconstructed_arrival_times);
+        for (i = 0; i < num_reco; i++) {
+                struct mlispExtractedPulse expulse;
+                expulse = mlispExtractedPulseVector_at(&extraction_pipeline, i);
+                reconstructed_arrival_times[i] = (
+                    (double)expulse.arrival_time_slice*
+                    time_slice_duration);
+        }
 
-    std::vector<double> reconstructed_arrival_times;
-    for (signal_processing::ExtractedPulse &pulse : raw.at(0))
-        reconstructed_arrival_times.push_back(
-            pulse.arrival_time_slice * time_slice_duration);
+        reco_mean = mli_mean(reconstructed_arrival_times, num_reco);
+        reco_std = mli_std(reconstructed_arrival_times, num_reco, reco_mean);
 
-    CHECK(arrival_time_std == Approx(ml::numeric::stddev(reconstructed_arrival_times)).margin(1e-10));
-    CHECK(true_arrival_time == Approx(ml::numeric::mean(reconstructed_arrival_times)).margin(1e-10));
+        CHECK_MARGIN(arrival_time_std, reco_std, 1e-10);
+        CHECK_MARGIN(true_arrival_time, reco_mean, 1e-10);
+
+        mlispPulseVector_free(&pulse_pipeline);
+        mlispExtractedPulseVector_free(&extraction_pipeline);
+        free(reconstructed_arrival_times);
 }
-*/
